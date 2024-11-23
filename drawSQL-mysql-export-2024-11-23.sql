67d76453b33c9e240fcd67c7a588dc872029b6e7
Vistoria-Veicular <vistoriaveicular@outlook.com.br>
2024-11-23 18:11:00 -0300

    .

--- drawSQL-mysql-export-2024-11-23.sql
+++ drawSQL-mysql-export-2024-11-23.sql
@@ -24,7 +24,7 @@ CREATE TABLE `Usuarios`(
     `Permissao` INT NOT NULL,
     `Ultimo_Login` DATETIME NOT NULL,
     `Final_Usuario` INT NOT NULL,
-    `Id_Pessoa` INT NOT NULL
+    `Id_Pessoa` UNSIGNED INT NOT NULL
 );
 CREATE TABLE `Veiculos`(
     `Id_Veiculo` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -34,14 +34,14 @@ CREATE TABLE `Veiculos`(
     `placa` VARCHAR(255) NOT NULL,
     `ultima_verificacao` DATETIME NOT NULL,
     `Status` INT NOT NULL,
-    `Id_condutor` INT NOT NULL
+    `Id_condutor` UNSIGNED INT NOT NULL
 );
 CREATE TABLE `Verificacao`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Id_Patio` INT NOT NULL,
-    `Id_Veiculo` INT NOT NULL,
-    `Id_Inspetor` INT NOT NULL,
-    `Id_Condutor` INT NOT NULL,
+    `Id_Patio` UNSIGNED INT NOT NULL,
+    `Id_Veiculo` UNSIGNED INT NOT NULL,
+    `Id_Inspetor` UNSIGNED INT NOT NULL,
+    `Id_Condutor` UNSIGNED INT NOT NULL,
     `Arquivo_relatorio` VARCHAR(255) NOT NULL,
     `Status` INT NOT NULL,
     `Resultado` INT NOT NULL
@@ -55,7 +55,7 @@ CREATE TABLE `Patio`(
     `Logradouro` VARCHAR(255) NOT NULL,
     `Numero` INT NOT NULL,
     `Complemento` VARCHAR(255) NOT NULL,
-    `Id_Responsavel` INT NOT NULL,
+    `Id_Responsavel` UNSIGNED INT NOT NULL,
     `Arquivo_FTP` VARCHAR(255) NOT NULL,
     `Arquivo_Server` VARCHAR(255) NOT NULL
 );
@@ -64,28 +64,18 @@ CREATE TABLE `Calendario`(
     `Id_Usuario` INT NOT NULL,
     `Id_Patio` INT NOT NULL,
     `Tipo_Agendamento` INT NOT NULL,
-    `Id_Veiculo` INT NOT NULL,
+    `Id_Veiculo` UNSIGNED INT NOT NULL,
     `Data` DATE NOT NULL,
     `Hora` TIME NOT NULL,
     `Status` INT NOT NULL
 );
-ALTER TABLE
-    `Calendario` ADD CONSTRAINT `calendario_id_veiculo_foreign` FOREIGN KEY(`Id_Veiculo`) REFERENCES `Veiculos`(`Id_Veiculo`);
-ALTER TABLE
-    `Verificacao` ADD CONSTRAINT `verificacao_id_veiculo_foreign` FOREIGN KEY(`Id_Veiculo`) REFERENCES `Veiculos`(`Id_Veiculo`);
-ALTER TABLE
-    `Patio` ADD CONSTRAINT `patio_id_responsavel_foreign` FOREIGN KEY(`Id_Responsavel`) REFERENCES `Pessoas`(`id`);
-ALTER TABLE
-    `Usuarios` ADD CONSTRAINT `usuarios_id_pessoa_foreign` FOREIGN KEY(`Id_Pessoa`) REFERENCES `Pessoas`(`id`);
-ALTER TABLE
-    `Verificacao` ADD CONSTRAINT `verificacao_id_inspetor_foreign` FOREIGN KEY(`Id_Inspetor`) REFERENCES `Pessoas`(`id`);
-ALTER TABLE
-    `Verificacao` ADD CONSTRAINT `verificacao_id_condutor_foreign` FOREIGN KEY(`Id_Condutor`) REFERENCES `Pessoas`(`id`);
-ALTER TABLE
-    `Calendario` ADD CONSTRAINT `calendario_id_usuario_foreign` FOREIGN KEY(`Id_Usuario`) REFERENCES `Pessoas`(`id`);
-ALTER TABLE
-    `Verificacao` ADD CONSTRAINT `verificacao_id_patio_foreign` FOREIGN KEY(`Id_Patio`) REFERENCES `Patio`(`id`);
-ALTER TABLE
-    `Veiculos` ADD CONSTRAINT `veiculos_id_condutor_foreign` FOREIGN KEY(`Id_condutor`) REFERENCES `Pessoas`(`id`);
-ALTER TABLE
-    `Calendario` ADD CONSTRAINT `calendario_id_patio_foreign` FOREIGN KEY(`Id_Patio`) REFERENCES `Patio`(`id`);
+ALTER TABLE `Calendario` ADD CONSTRAINT `calendario_id_veiculo_foreign` FOREIGN KEY(`Id_Veiculo`) REFERENCES `Veiculos`(`Id_Veiculo`);
+ALTER TABLE `Verificacao` ADD CONSTRAINT `verificacao_id_veiculo_foreign` FOREIGN KEY(`Id_Veiculo`) REFERENCES `Veiculos`(`Id_Veiculo`);
+ALTER TABLE `Patio` ADD CONSTRAINT `patio_id_responsavel_foreign` FOREIGN KEY(`Id_Responsavel`) REFERENCES `Pessoas`(`id`);
+ALTER TABLE `Usuarios` ADD CONSTRAINT `usuarios_id_pessoa_foreign` FOREIGN KEY(`Id_Pessoa`) REFERENCES `Pessoas`(`id`);
+ALTER TABLE `Verificacao` ADD CONSTRAINT `verificacao_id_inspetor_foreign` FOREIGN KEY(`Id_Inspetor`) REFERENCES `Pessoas`(`id`);
+ALTER TABLE `Verificacao` ADD CONSTRAINT `verificacao_id_condutor_foreign` FOREIGN KEY(`Id_Condutor`) REFERENCES `Pessoas`(`id`);
+ALTER TABLE `Calendario` ADD CONSTRAINT `calendario_id_usuario_foreign` FOREIGN KEY(`Id_Usuario`) REFERENCES `Pessoas`(`id`);
+ALTER TABLE `Verificacao` ADD CONSTRAINT `verificacao_id_patio_foreign` FOREIGN KEY(`Id_Patio`) REFERENCES `Patio`(`id`);
+ALTER TABLE `Veiculos` ADD CONSTRAINT `veiculos_id_condutor_foreign` FOREIGN KEY(`Id_condutor`) REFERENCES `Pessoas`(`id`);
+ALTER TABLE `Calendario` ADD CONSTRAINT `calendario_id_patio_foreign` FOREIGN KEY(`Id_Patio`) REFERENCES `Patio`(`id`);
